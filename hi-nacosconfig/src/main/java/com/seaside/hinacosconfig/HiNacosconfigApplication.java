848488c3cd5bb8d3b39d2891a5d0e9dc8576cc12
635722800@qq.com <Konglingfan07!!?>
2022-04-17 22:54:35 +0800

    SpringCloudAlibaba nacos config

---
+++ hi-nacosconfig/src/main/java/com/seaside/hinacosconfig/HiNacosconfigApplication.java
@@ -0,0 +1,19 @@
+package com.seaside.hinacosconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class HiNacosconfigApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(HiNacosconfigApplication.class, args);
+        String userName = applicationContext.getEnvironment().getProperty("user.name");
+        String userAge = applicationContext.getEnvironment().getProperty("user.age");
+        System.err.println("user name :"+userName+"; age: "+userAge);
+    }
+
+}
